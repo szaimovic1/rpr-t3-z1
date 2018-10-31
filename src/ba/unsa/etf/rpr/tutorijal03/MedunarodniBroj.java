17e4bdff487d07983a43ca0b689e901d2f0944fd
szaimovic1 <szaimovic1@etf.unsa.ba>
2018-10-31 03:33:05 +0100

    zavrsen dio za TelefonskiBroj

---
+++ src/ba/unsa/etf/rpr/tutorijal03/MedunarodniBroj.java
@@ -0,0 +1,11 @@
+package ba.unsa.etf.rpr.tutorijal03;
+
+public class MedunarodniBroj {
+    private String drzava;
+    private String broj;
+
+    public MedunarodniBroj(String drzava, String broj) {
+        this.drzava=drzava;
+        this.broj = broj;
+    }
+}
