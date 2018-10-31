fbd0ac6733d167bfb8cd089fd810b1c6bb8bf222
szaimovic1 <szaimovic1@etf.unsa.ba>
2018-10-31 03:22:35 +0100

    ispunjena priprema

---
+++ src/ba/unsa/etf/rpr/tutorijal03/FiksniBroj.java
@@ -0,0 +1,30 @@
+package ba.unsa.etf.rpr.tutorijal03;
+
+enum Grad{
+    TRAVNIK, ORASJE, ZENICA, SARAJEVO, LIVNO, TUZLA, MOSTAR,                //mogu li enumu dodijeliti dr. vrijednosti???
+    BIHAC, GORAZDE, SIROKIBRIJEG, BRCKO
+}
+
+public class FiksniBroj extends TelefonskiBroj {
+    private Grad grad;
+    private String broj;
+
+    public FiksniBroj(Grad grad, String broj) {                                                             //generate..
+        this.grad = grad;
+        this.broj = broj;
+    }
+
+    @Override
+    public String toString(){
+        String s;
+        if(grad!=Grad.BRCKO)
+            s = '03' + grad.ordinal() + '/' + broj;
+        else s = '049/' + broj;
+        return s;
+    }
+
+    @Override
+    public String ispisi() {
+        System.out.println(this);
+    }
+}
---
+++ src/ba/unsa/etf/rpr/tutorijal03/MobilniBroj.java
@@ -0,0 +1,13 @@
+package ba.unsa.etf.rpr.tutorijal03;
+
+public class  MobilniBroj extends TelefonskiBroj {
+    private int MobilnaMreza;
+    private String broj;
+
+    public MobilniBroj(int mobilnaMreza, String broj) {
+        MobilnaMreza = mobilnaMreza;
+        this.broj = broj;
+    }
+
+
+}
