29f1aa8d4d50a93c617413c35c0d92f8796c2caf
szaimovic1 <szaimovic1@etf.unsa.ba>
2018-11-07 10:48:14 +0100

    Zabilježena upozorenja!

--- src/ba/unsa/etf/rpr/tutorijal03/Imenik.java
+++ src/ba/unsa/etf/rpr/tutorijal03/Imenik.java
@@ -10,12 +10,12 @@ public class Imenik {
         imenik = new HashMap<String, TelefonskiBroj>();
     }
 
-    public void dodaj(String ime, TelefonskiBroj broj){
+    public void dodaj(String ime, TelefonskiBroj broj){                       //warning package-private???
         imenik.put(ime, broj);
     }
 
     public String dajBroj(String ime){
-        return imenik.get(ime).ispisi();                                                               //???
+        return imenik.get(ime).ispisi();                                                              //???
     }
 
     public String dajIme(TelefonskiBroj broj){
@@ -27,20 +27,20 @@ public class Imenik {
     }
 
     public String naSlovo(char s){
-        String spisak = new String();
+        String spisak = new String();                                          //warning??
         int i=1;
         for (HashMap.Entry<String, TelefonskiBroj> x : imenik.entrySet())
             if(x.getKey().charAt(0)==s) {
-                spisak+=i + ". " + x.getKey() + " - " + x.getValue().ispisi() + "\n";
+                spisak+=i + ". " + x.getKey() + " - " + x.getValue().ispisi() + "\n";         //warning?
                 i++;
             }
         return spisak;
     }
 
-    public Set<String> izGrada(FiksniBroj.Grad g){
+    public Set<String> izGrada(FiksniBroj.Grad g){                                             //i opet package..
         TreeSet<String> spisak = new TreeSet<String>();
         for (HashMap.Entry<String, TelefonskiBroj> x : imenik.entrySet()) {
-            String s = new String(x.getValue().ispisi());
+            String s = new String(x.getValue().ispisi());                                         //dosta warninga...
             /*FiksniBroj.Grad pozivni = new FiksniBroj.Grad((String)(s[0] + s[1] + s[2]));*/          //nesto pametnije
             int br = Character.getNumericValue(s.charAt(2));
             if(br==g.ordinal())                                                               //but 39 i 49 problem
@@ -52,9 +52,9 @@ public class Imenik {
 
     public Set<TelefonskiBroj> izGradaBrojevi(FiksniBroj.Grad g){
         Comparator<TelefonskiBroj> komparator = (TelefonskiBroj  broj1, TelefonskiBroj  broj2) -> (broj1.ispisi().compareTo(broj2.ispisi()));
-        SortedSet<TelefonskiBroj> spisak = new TreeSet(komparator);
+        SortedSet<TelefonskiBroj> spisak = new TreeSet(komparator);                       //upozorenje za dodjelu????
         for (HashMap.Entry<String, TelefonskiBroj> x : imenik.entrySet()) {
-            String s = new String(x.getValue().ispisi());
+            String s = new String(x.getValue().ispisi());                                 //redundant????
             int br = Character.getNumericValue(s.charAt(2));
             if(br==g.ordinal())                                                            //Comparable?????????????????
                 spisak.add(x.getValue());
